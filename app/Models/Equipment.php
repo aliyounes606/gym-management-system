d7f4cada14e6b60738d8edaf07ff151a7a6ab247
kinda <kindaalabdullah34@gmail.com>
2025-12-29 21:34:41 +0300

    create model

---
+++ app/Models/Equipment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Equipment extends Model
+{
+    protected $table = 'equipment';
+
+    protected $fillable = [
+        'name',
+        'status',
+        'quantity',
+    ];
+
+}
