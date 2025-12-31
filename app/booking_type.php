cf7be99c6484c99d99c4d53a6bcb93708d3266e6
Amr2005A <amrmohissen.b@egmail.com>
2025-12-31 21:41:44 +0300

    move the enums to bookings migration and remov them from add folder

--- app/booking_type.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-enum booking_type: string 
-{
-    case session = 'session';
-    case courses = 'courses';
-}
--- app/payment_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-enum payment_status: string 
-{
-    case paid = 'paid';
-    case pending = 'pending';
-}
